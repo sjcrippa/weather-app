53e21663717fb5a61af61d9576d1f62563869c36
Santiago Crippa <santicrippa@gmail.com>
2023-04-03 09:32:37 -0300

    adding scripts and imgs

---
+++ index.js
@@ -0,0 +1,35 @@
+const container = document.querySelector('.container');
+const search = document.querySelector('.search-box button');
+const weatherBox = document.querySelector('.weather-box');
+const weatherDetails = document.querySelector('.weather-details');
+const error404 = document.querySelector('.not-found');
+
+search.addEventListener('click', () => {
+
+    const APIKey = '1bdb5f9889f6ad70cd76e3cc5c6bc940';
+    const city = document.querySelector('.search-box input').value;
+
+    if(city === '');
+        return;
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric?appid=${APIKey}`).then(response => response.json()).then(json => {
+
+        if(json.cod === '404'){
+            container.style.height = '400px';
+            weatherBox.style.display = 'none';
+            weatherDetails.style.display = 'none';
+            error404.style.display = 'block';
+            error404.classList.add('fadeIn');
+            return;
+        }
+
+        error404.style.display = 'none';
+        error404.classList.remove('fadeIn');
+
+        const image = document.querySelector('.weather-box img')
+        const temperature = document.querySelector('.weather-box .temperature');
+        const description = document.querySelector('.weather-box .description');
+        const humidity = document.querySelector('.weather-box .humidity span');
+        const wind = document.querySelector('.weather-box .wind span');
+    })
+
+})
